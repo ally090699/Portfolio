fcf01d8da4b599cc3f70dcf43cc20c0f8358a748
Allison So <aso43@uwo.ca>
2025-02-08 03:48:49 -0500

    Updates

---
+++ precache-manifest.a36a32c7efa8edb210d7b6c44b91bfd9.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "054cb98154fd6385e84067104aa4f648",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f48461be29b4211a728b",
+    "url": "/Portfolio/static/css/main.2f1bef46.chunk.css"
+  },
+  {
+    "revision": "18c686e05f3e08e1394b",
+    "url": "/Portfolio/static/js/2.727331d3.chunk.js"
+  },
+  {
+    "revision": "f48461be29b4211a728b",
+    "url": "/Portfolio/static/js/main.dda91028.chunk.js"
+  },
+  {
+    "revision": "49aa45ff9ca345ba2525",
+    "url": "/Portfolio/static/js/runtime-main.f506e4e2.js"
+  },
+  {
+    "revision": "a80626c97c9e43a4a1e1fc902562d279",
+    "url": "/Portfolio/static/media/Minesweeper.a80626c9.png"
+  },
+  {
+    "revision": "a7e4b5aac03308f7f1ef08a5e116300b",
+    "url": "/Portfolio/static/media/Untitled_Artwork 13.a7e4b5aa.jpeg"
+  },
+  {
+    "revision": "d716e4b34d40c0682df02b27eb5b77d7",
+    "url": "/Portfolio/static/media/Untitled_Artwork 2.d716e4b3.jpeg"
+  },
+  {
+    "revision": "d97874598490f905c535a5f46b58a825",
+    "url": "/Portfolio/static/media/Untitled_Artwork.d9787459.jpeg"
+  },
+  {
+    "revision": "278c34f1c877f8dff2044e1e303024a4",
+    "url": "/Portfolio/static/media/coding_in_action.278c34f1.png"
+  },
+  {
+    "revision": "1013ab58d77acb8732a537cc4d7f6d34",
+    "url": "/Portfolio/static/media/crochetinaction.1013ab58.png"
+  },
+  {
+    "revision": "d719523e630432dab46a4f76467b2743",
+    "url": "/Portfolio/static/media/designing_in_action.d719523e.png"
+  },
+  {
+    "revision": "20defe3adb25649b19d14f4da063f83a",
+    "url": "/Portfolio/static/media/graphic_design_OCA1.20defe3a.png"
+  },
+  {
+    "revision": "e5c0ccfe4f126948d3bb518b5bd9bb8a",
+    "url": "/Portfolio/static/media/graphic_design_OCA2.e5c0ccfe.png"
+  },
+  {
+    "revision": "e59bac4f76eb8d35606d23832aeedd07",
+    "url": "/Portfolio/static/media/graphic_design_RDR.e59bac4f.png"
+  },
+  {
+    "revision": "ee31f8370c51289ebf4046605fc68674",
+    "url": "/Portfolio/static/media/graphic_design_RDR2.ee31f837.jpg"
+  },
+  {
+    "revision": "74fc3e75a3c0903d43c6262c3f73cd51",
+    "url": "/Portfolio/static/media/graphic_design_RDR3.74fc3e75.jpg"
+  },
+  {
+    "revision": "bcfb5cfc689ef23f81496f7ba8196411",
+    "url": "/Portfolio/static/media/kitCollective.bcfb5cfc.png"
+  },
+  {
+    "revision": "21cc32fd55bd42ffa5a8b8629864067b",
+    "url": "/Portfolio/static/media/travel.21cc32fd.png"
+  }
+]);
