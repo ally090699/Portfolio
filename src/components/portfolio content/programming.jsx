d72ee10f306fe892b9c148fed2e4b0bcbdcef472
Allison So <aso43@uwo.ca>
2025-03-18 17:02:24 -0400

    updated first draft of lamp stack kit collective

--- src/components/portfolio content/programming.jsx
+++ src/components/portfolio content/programming.jsx
@@ -98,8 +98,8 @@ const programming = [
         subtitle: "Full Stack Web Development",
         img: img10,
         stack: ["Linux/macOS", "Apache", "MySQL", "MariaDB", "AWS RDS", "PHP"],
-        text: "An ongoing full-stack e-commerce and CMS platform project built using the LAMP stack, with a MariaDB database hosted on AWS RDS. Currently, the project focuses on implementing the customer-facing side, including basic user authentication. The admin side for the CMS is still under development. The project is not yet deployed, but the code is available on GitHub to view progress until deployment on Render.",
-        url: "https://github.com/ally090699/lamp-kit-collective",
+        text: "Kit Collective 4.0 is a fully functional full-stack e-commerce and CMS platform built using the LAMP stack, with a MariaDB database hosted on AWS RDS and deployed on an EC2 instance within a VPC. This latest version includes significant improvements over previous iterations, enhancing both functionality and user experience.",
+        url: "http://18.218.216.21/lamp-kit-collective/public/index.php",
     },
 ];
 
